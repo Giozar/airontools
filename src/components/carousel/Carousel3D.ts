d90e02d37312d6bda01fd59a99914671f42e0f80
Citlalli <citlalli688@outlook.com>
2024-09-07 10:12:25 -0600

    add: modificacion de carousel para que fuera draggable

--- src/components/carousel/Carousel3D.ts
+++ src/components/carousel/Carousel3D.ts
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const items: HTMLDivElement[] = Array.from(document.querySelectorAll('.carousel-item'));
     const numItems: number = items.length;
     let rotation: number = 0;
+    let isDragging: boolean = false;
+    let startX: number = 0;
+    let startRotation: number = 0;
   
     if (!carousel) {
       console.error('Carousel element not found');
@@ -71,6 +74,37 @@ document.addEventListener('DOMContentLoaded', () => {
       event.preventDefault();
       updateRotation(event.deltaY * 0.1);
     };
+    /**
+     * Manejador del evento drag start para rotar el carrusel.
+     * @param event - Evento de click de ratón.
+     */
+    const handleDragStart = (event: MouseEvent): void => {
+      isDragging = true;
+      startX = event.clientX;
+      startRotation = rotation;
+      document.addEventListener('mousemove', handleDragMove);
+      document.addEventListener('mouseup', handleDragEnd);
+    };
+    /**
+     * Manejador del evento drag move para rotar el carrusel.
+     * @param event - Evento de click de ratón.
+     */
+    const handleDragMove = (event: MouseEvent): void => {
+      if (!isDragging) return;
+      const deltaX = event.clientX - startX;
+      const rotationDelta = deltaX * 0.5; // Ajusta este valor para cambiar la sensibilidad
+      rotation = startRotation + rotationDelta;
+      carousel.style.transform = `rotateY(${rotation}deg)`;
+    };
+    /**
+     * Termina el evento de rotar el carrusel.
+     */
+    const handleDragEnd = (): void => {
+      if (!isDragging) return;
+      isDragging = false;
+      document.removeEventListener('mousemove', handleDragMove);
+      document.removeEventListener('mouseup', handleDragEnd);
+    };
   
     // Inicializar el carrusel
     updateCarousel();
@@ -79,8 +113,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const carouselContainer: HTMLElement | null = document.getElementById('carousel-container');
     if (carouselContainer) {
       carouselContainer.addEventListener('wheel', handleWheel);
+      carouselContainer.addEventListener('mousedown', handleDragStart);
     } else {
       console.error('Carousel container element not found');
     }
   });
-  
